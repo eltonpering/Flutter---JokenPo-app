e7690121cd3aa090369b0e1c739c0f89e9e4c5d7
Elton A Pering <elton_pering@adsomos.com>
2020-08-31 17:15:51 -0300

    remove comment

--- lib/Jogo.dart
+++ lib/Jogo.dart
@@ -115,11 +115,6 @@ class _JogoState extends State<Jogo> {
                   height: 100,
                 ),
               )
-              /*
-              Image.asset("imagens/pedra.png", height: 100,),
-              Image.asset("imagens/papel.png", height: 100,),
-              Image.asset("imagens/tesoura.png", height: 100,)
-              */
             ],
           )
         ],
